c756375cfd7ac1077e2a0e3881685f7e85ecd712
ParrotShen <parrotshen@yahoo.com.tw>
2019-02-11 10:50:54 +0800

    add ncurses example

---
+++ ncurses/my_ncurses.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <curses.h>
+
+static WINDOW *_win = NULL;
+
+int init_conio(void)
+{
+    _win = initscr();
+
+    if (NULL == _win)
+    {
+        fprintf(stderr, "ERR: fail to initial ncurses\n\n");
+        return -1;
+    }
+
+	keypad(stdscr, TRUE);
+
+	noecho();
+
+    return 0;
+}
+
+void uninit_conio(void)
+{
+    delwin( _win );
+
+    endwin();
+
+    refresh();
+}
+
+void gotoxy(int x, int y)
+{
+    wmove(_win, y, x);
+
+    wrefresh( _win );
+}
+
+int wherex(void)
+{
+    int x = 0;
+    int y = 0;
+
+    getyx(_win, y, x);
+
+    return x;
+}
+
+int wherey(void)
+{
+    int x = 0;
+    int y = 0;
+
+    getyx(_win, y, x);
+
+    return y;
+}
+
+
+int main(int argc, char *argv[])
+{
+    int x = 10;
+    int y = 20;
+
+    if (0 == init_conio())
+    {
+        printw("goto (%d, %d)\n", x, y);
+        refresh();
+
+        sleep(1);
+
+        gotoxy(x, y);
+
+		attron( A_STANDOUT );
+        printw("Hello, world!\n");
+		attroff( A_STANDOUT );
+        refresh();
+
+        sleep(1);
+
+        printw("now at (%d, %d)\n", wherex(), wherey());
+        refresh();
+
+        uninit_conio();
+    }
+
+    return 0;
+}
+
---
+++ time/gmtime.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    struct tm *nPtr;
+    time_t t;
+
+    if (argc < 2)
+    {
+        printf("Usage: gmtime INTEGER_VALUE\n");
+        printf("\n");
+        return 0;
+    }
+
+    t = (time_t)atoi( argv[1] );
+    nPtr = gmtime( &t );
+
+    printf("tm_sec  = %d\n", nPtr->tm_sec);          /* 0 ~ 59 */
+    printf("tm_min  = %d\n", nPtr->tm_min);          /* 0 ~ 59 */
+    printf("tm_hour = %d\n", nPtr->tm_hour);         /* 0 ~ 23 */
+    printf("tm_mday = %d\n", nPtr->tm_mday);         /* 1 ~ 31 */
+    printf("tm_mon  = %d\n", nPtr->tm_mon + 1);      /* 0 ~ 11 */
+    printf("tm_year = %d\n", nPtr->tm_year + 1900);  /* year - 1900 */
+    printf("tm_wday = %d\n", nPtr->tm_wday);         /* 0 ~ 6 */
+    printf("tm_yday = %d\n", nPtr->tm_yday + 1);     /* 0 ~ 365 */
+    
+	return 0;
+}
+
